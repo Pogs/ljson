a0a1577b6ff5262075ab5e097d5766e3fc86c251
Poggles <sir.pogsalot@gmail.com>
2013-04-21 23:39:08 +0000

    changed too many things...
    changes:
    	- error on encoding circular references
    	- support for JS unicode escapes (\uFFFF)
    	- NaN -> null & inf -> null (like original JSON impl.)
    	- improved lua table to json array detection
    	- tiny speed up with multi-byte peek() & read()
    	- on decode: report exact location of syntax error
    	- formatting (sue me)

--- src/jsua.lua
+++ src/jsua.lua
@@ -2,295 +2,497 @@ local jsua = {}
 
 jsua.null = {}
 
+local numchars = '-0123456789.eE'
+
+local escapes =
+{
+	['\\'] = '\\',
+	['/']  = '/',
+	['"']  = '"',
+	b      = '\b',
+	f      = '\f',
+	n      = '\n',
+	r      = '\r',
+	t      = '\t'
+}
+
 local impl = {}
 
-impl.array = {}
-impl.object = {}
+impl.array  = {} -- only used for
+impl.object = {} -- identity matching
 
-local escapes = {["\""] = "\"", ["\\"] = "\\", ["/"] = "/", b = "\b",
-                 f = "\f", n = "\n", r = "\r", t = "\t"}
+impl.is_whitespace_char = function (c) return string.find(' \n\r\t',  c, 1, true) ~= nil end
+impl.is_number_char     = function (c) return string.find(numchars,   c, 1, true) ~= nil end
 
-function impl.is_number_char(char)
-    return string.find("-0123456789.eE", char, 1, true) ~= nil
-end
+impl.read =
+	function (peek, read)
+		impl.skip_whitespace(peek, read)
 
-function impl.is_whitespace_char(char)
-    return string.find(" \n\r\t", char, 1, true) ~= nil
-end
+		local value = impl.read_value(peek, read)
 
-function impl.read(peek_char, read_char)
-    impl.skip_whitespace(peek_char, read_char)
-    local value = impl.read_value(peek_char, read_char)
-    impl.skip_whitespace(peek_char, read_char)
-    assert(not peek_char())
-    return value
-end
+		impl.skip_whitespace(peek, read)
 
-function impl.read_value(peek_char, read_char)
-    local char = peek_char()
-    if char == "\"" then
-        return impl.read_string(peek_char, read_char)
-    elseif impl.is_number_char(char) then
-        return impl.read_number(peek_char, read_char)
-    elseif char == "{" then
-        return impl.read_object(peek_char, read_char)
-    elseif char == "[" then
-        return impl.read_array(peek_char, read_char)
-    elseif char == "t" then
-        return impl.read_constant(peek_char, read_char, "true", true)
-    elseif char == "f" then
-        return impl.read_constant(peek_char, read_char, "false", false)
-    elseif char == "n" then
-        return impl.read_constant(peek_char, read_char, "null", jsua.null)
-    else
-        assert(false)
-    end
-end
+		assert(not peek())
 
-function impl.skip_whitespace(peek_char, read_char)
-    while true do
-        local char = peek_char()
-        if not char then
-            break
-        elseif char == "/" then
-            impl.skip_comment(peek_char, read_char)
-        elseif impl.is_whitespace_char(char) then
-            read_char()
-        else
-            break
-        end
-    end
-end
+		return value
+	end
 
-function impl.skip_comment(peek_char, read_char)
-    local char = read_char()
-    assert(char == "/")
-    char = read_char()
-    if char == "/" then
-        repeat
-            char = read_char()
-        until not char or char == "\n"
-        return nil
-    elseif char == "*" then
-        local star = false
-        while true do
-            char = read_char()
-            assert(char)
-            if char == "*" then
-                star = true
-            elseif star and char == "/" then
-                return nil
-            else
-                star = false
-            end
-        end
-    else
-        assert(false)
-    end
-end
+impl.read_value =
+	function (peek, read)
+		local char = peek()
 
-function impl.read_string(peek_char, read_char)
-    local buffer = {}
-    local char = read_char()
-    assert(char == "\"")
-    while true do
-        char = read_char()
-        assert(char)
-        if char == "\"" then
-            break
-        elseif char == "\\" then
-            char = read_char()
-            if escapes[char] then
-                table.insert(buffer, escapes[char])
-            elseif char == "u" then
-                -- JSON unicode escapes are not supported.
-                assert(false)
-            else
-                assert(false)
-            end
-        else
-            table.insert(buffer, char)
-        end
-    end
-    impl.skip_whitespace(peek_char, read_char)
-    return table.concat(buffer)
-end
+		if     char == '"'               then return impl.read_string  (peek, read)
+		elseif char == '{'               then return impl.read_object  (peek, read)
+		elseif char == '['               then return impl.read_array   (peek, read)
+		elseif char == 't'               then return impl.read_constant(peek, read, 'true',  true     )
+		elseif char == 'f'               then return impl.read_constant(peek, read, 'false', false    )
+		elseif char == 'n'               then return impl.read_constant(peek, read, 'null',  jsua.null)
+		elseif impl.is_number_char(char) then return impl.read_number  (peek, read)
+		else
+			assert()
+		end
+	end
 
-function impl.read_number(peek_char, read_char)
-    local buffer = {}
-    while peek_char() and impl.is_number_char(peek_char()) do
-        table.insert(buffer, read_char())
-    end
-    impl.skip_whitespace(peek_char, read_char)
-    return tonumber(table.concat(buffer))
-end
+impl.skip_whitespace =
+	function (peek, read)
+		while true do
+			local char = peek()
 
-function impl.read_object(peek_char, read_char)
-    local obj = jsua.new_object()
-    local char = read_char()
-    assert(char == "{")
-    while true do
-        impl.skip_whitespace(peek_char, read_char)
-        char = peek_char()
-        assert(char)
-        if char == "}" then
-            read_char()
-            break
-        end
-        local key = impl.read_value(peek_char, read_char)
-        impl.skip_whitespace(peek_char, read_char)
-        local char = read_char()
-        assert(char == ":")
-        impl.skip_whitespace(peek_char, read_char)
-        local value = impl.read_value(peek_char, read_char)
-        obj[key] = value
-        char = read_char()
-        if char == "}" then
-            break
-        end
-        assert(char == ",")
-    end
-    impl.skip_whitespace(peek_char, read_char)
-    return obj
-end
+			if not char then
+				break
+			elseif char == '/' then
+				impl.skip_comment(peek, read)
+			elseif impl.is_whitespace_char(char) then
+				read()
+			else
+				break
+			end
+		end
+	end
 
-function impl.read_array(peek_char, read_char)
-    local arr = jsua.new_array()
-    local char = read_char()
-    assert(char == "[")
-    while true do
-        impl.skip_whitespace(peek_char, read_char)
-        char = peek_char()
-        assert(char)
-        if char == "]" then
-            read_char()
-            break
-        end
-        table.insert(arr, impl.read_value(peek_char, read_char))
-        impl.skip_whitespace(peek_char, read_char)
-        char = read_char()
-        assert(char)
-        if char == "]" then
-            break
-        end
-        assert(char == ",")
-    end
-    impl.skip_whitespace(peek_char, read_char)
-    return arr
-end
+impl.skip_comment =
+	function (peek, read)
+		local char = read()
 
-function impl.read_constant(peek_char, read_char, name, value)
-    local i = 1
-    while i <= #name do
-        local char = read_char()
-        assert(char == string.sub(name, i, i))
-        i = i + 1
-    end
-    impl.skip_whitespace(peek_char, read_char)
-    return value
-end
+		assert(char == '/')
 
-function impl.write_value(value, write_string)
-    if type(value) == "string" then
-        write_string(string.format("%q", value))
-    elseif type(value) == "number" then
-        write_string(tostring(value))
-    elseif jsua.is_array(value) then
-        impl.write_array(value, write_string)
-    elseif jsua.is_object(value) then
-        impl.write_object(value, write_string)
-    elseif type(value) == "boolean" then
-        write_string(tostring(value))
-    elseif jsua.is_null(value) then
-        write_string("null")
-    else
-        assert(false)
-    end
-end
+		char = read()
 
-function impl.write_array(arr, write_string)
-    write_string("[")
-    for i, value in ipairs(arr) do
-        if i >= 2 then
-            write_string(", ")
-        end
-        impl.write_value(value, write_string)
-    end
-    write_string("]")
-end
+		if char == '/' then
+			read('.-\n')
+			return
+		elseif char == '*' then
+			local star = false
 
-function impl.write_object(obj, write_string)
-    write_string("{")
-    local keys = {}
-    for key, _ in pairs(obj) do
-        table.insert(keys, key)
-    end
-    table.sort(keys)
-    for i, key in ipairs(keys) do
-        if i >= 2 then
-            write_string(", ")
-        end
-        impl.write_value(key, write_string)
-        write_string(": ")
-        impl.write_value(obj[key], write_string)
-    end
-    write_string("}")
-end
+			while true do
+				char = assert(read())
 
-function jsua.new_array(arr)
-    arr = arr or {}
-    setmetatable(arr, impl.array)
-    return arr
-end
+				if char == '*' then
+					star = true
+				elseif star and char == '/' then
+					return
+				else
+					star = false
+				end
+			end
+		else
+			assert()
+		end
+	end
 
-function jsua.new_object(obj)
-    obj = obj or {}
-    setmetatable(obj, impl.object)
-    return obj
-end
+local utf8_encode =
+	function (cp)
+		local n, mask = nil, nil
 
-function jsua.is_array(value)
-    local t = type(value)
-    local mt = getmetatable(value)
-    return t == "table" and value ~= jsua.null and
-            (mt == impl.array or mt == nil and value[1] ~= nil)
-end
+		if cp < 0x80 then
+			return string.char(cp)
+		elseif i < 0x800 then
+			n, mask = 2, 0xC0
+		else
+			n, mask = 3, 0xE0
+		end
 
-function jsua.is_null(value)
-    return value == nil or value == jsua.null
-end
+		local bytes = {}
 
-function jsua.is_object(value)
-    local t = type(value)
-    local mt = getmetatable(value)
-    return t == "table" and value ~= jsua.null and
-            (mt == impl.object or mt == nil and value[1] == nil)
-end
+		while n > 1 do
+			bytes[n] = string.char(0x80 + bit32.band(i, 0x3F))
+			i = bit32.rshift(i, 6)
+			n = n - 1
+		end
 
-function jsua.read(str)
-    local pos = 1
-    local function peek_char()
-        return pos <= #str and string.sub(str, pos, pos) or nil
-    end
-    local function read_char()
-        local char = peek_char()
-        pos = pos + 1
-        return char
-    end
-    local success, result = pcall(impl.read, peek_char, read_char)
-    if not success then
-        error("syntax error at character " .. pos)
-    end
-    return result
-end
+		return table.concat(bytes)
+	end
 
-function jsua.write(val)
-    local buffer = {}
-    local function write_string(str)
-        table.insert(buffer, str)
-    end
-    impl.write_value(val, write_string)
-    return table.concat(buffer)
-end
+impl.read_string =
+	function (peek, read)
+		local buf = {}
+
+		local char = read()
+
+		assert(char == '"')
+
+		while true do
+			char = assert(read())
+
+			if char == '"' then
+				break
+			elseif char == '\\' then
+				char = read()
+
+				if escapes[char] then
+					table.insert(buf, escapes[char])
+				elseif char == 'u' then
+					char = assert(read('%x%x%x%x'))
+
+					local tmp = tonumber(char, 16)
+
+					table.insert(utf8_encode(tmp))
+				else
+					table.insert(buf, char)
+				end
+			else
+				table.insert(buf, char)
+			end
+		end
+
+		impl.skip_whitespace(peek, read)
+
+		return table.concat(buf)
+	end
+
+impl.read_number =
+	function (peek, read)
+		local buf = {}
+
+		while peek() and impl.is_number_char(peek()) do
+			table.insert(buf, read())
+		end
+
+		impl.skip_whitespace(peek, read)
+
+		return tonumber(table.concat(buf))
+	end
+
+impl.read_number =
+	function (peek, read)
+		local num = read('[' .. numchars .. ']+')
+
+		impl.skip_whitespace(peek, read)
+
+		return assert(tonumber(num))
+	end
+
+impl.read_object =
+	function (peek, read)
+		local obj  = jsua.new_object()
+		local char = read()
+
+		assert(char == '{')
+
+		while true do
+			impl.skip_whitespace(peek, read)
+
+			char = assert(peek())
+
+			if char == '}' then
+				read()
+				break
+			end
+
+			local key = impl.read_value(peek, read)
+
+			impl.skip_whitespace(peek, read)
+
+			local char = read()
+
+			assert(char == ':')
+
+			impl.skip_whitespace(peek, read)
+
+			local value = impl.read_value(peek, read)
+
+			obj[key] = value
+
+			char = read()
+
+			if char == '}' then
+				break
+			end
+
+			assert(char == ',')
+		end
+
+		impl.skip_whitespace(peek, read)
+
+		return obj
+	end
+
+impl.read_array =
+	function (peek, read)
+		local arr = jsua.new_array()
+
+		local char = read()
+
+		assert(char == '[')
+
+		while true do
+			impl.skip_whitespace(peek, read)
+
+			char = assert(peek())
+
+			if char == ']' then
+				read()
+				break
+			end
+
+			table.insert(arr, impl.read_value(peek, read))
+
+			impl.skip_whitespace(peek, read)
+
+			char = read()
+
+			assert(char)
+
+			if char == ']' then
+				break
+			end
+
+			assert(char == ',')
+		end
+
+		impl.skip_whitespace(peek, read)
+
+		return arr
+	end
+
+impl.read_constant =
+	function (peek, read, name, value)
+		assert(read(name))
+
+		impl.skip_whitespace(peek, read)
+
+		return value
+	end
+
+impl.write_value =
+	function (value, write)
+		local t = type(value)
+
+		if     t == 'string'       then write(string.format('%q', value))
+		elseif t == 'boolean'      then write(tostring(value))
+		elseif t == 'number'       then impl.write_num(value, write)
+		elseif jsua.is_null(value) then write('null')
+		elseif t == 'table' then
+			if jsua.is_array(value) then
+				impl.write_array(value, write)
+			else
+				impl.write_object(value, write)
+			end
+		else
+			assert()
+		end
+	end
+
+impl.write_num =
+	function (num, write)
+		if math.abs(num) == math.huge or num ~= num then
+			write('null')
+			return
+		end
+
+		write(tostring(num))
+	end
+
+impl.write_array =
+	function (arr, write)
+		local len = #arr
+
+		write('[')
+
+		for i = 1, len - 1 do
+			local value = arr[i]
+
+			impl.write_value(value, write)
+			write(', ')
+		end
+
+		impl.write_value(arr[len], write)
+
+		write(']')
+	end
+
+local merge_numkeys =
+	function (obj)
+		local t = {}
+
+		for k, v in pairs(obj) do
+			obj[k] = nil
+			t[tostring(k)] = v
+		end
+
+		for k, v in pairs(t) do
+			obj[k] = v
+		end
+
+		return obj
+	end
+
+impl.write_object =
+	function (obj, write)
+		obj = merge_numkeys(obj)
+
+		write('{')
+
+		local keys = {}
+
+		for key in pairs(obj) do
+			table.insert(keys, key)
+		end
+
+		table.sort(keys)
+
+		local len = #keys
+
+		for i = 1, len - 1 do
+			local key = keys[i]
+
+			impl.write_value(key, write)
+			write(': ')
+			impl.write_value(obj[key], write)
+			write(', ')
+		end
+
+		impl.write_value(keys[len], write)
+		write(': ')
+		impl.write_value(obj[keys[len]], write)
+
+		write('}')
+	end
+
+jsua.new_array  = function (arr) return setmetatable(arr or {}, impl.array ) end
+jsua.new_object = function (obj) return setmetatable(obj or {}, impl.object) end
+
+jsua.is_array =
+	function (value)
+		local t  = type(value)
+		local mt = getmetatable(value)
+
+		if t ~= 'table' then
+			return false
+		end
+
+		if value == jsua.null then
+			return false
+		end
+
+		if mt and mt == impl.array then
+			return true
+		end
+
+		local keys = 0
+
+		for k in pairs(value) do
+			keys = keys + 1
+
+			if type(k) ~= 'number' then
+				return false
+			end
+		end
+
+		local sequential = 0
+
+		for i in ipairs(value) do
+			sequential = sequential + 1
+		end
+
+		if sequential < keys then
+			return false
+		end
+
+		return true
+	end
+
+jsua.is_null =
+	function (obj)
+		return obj == nil or obj == jsua.null
+	end
+
+jsua.read =
+	function (str)
+		local pos  = 1
+		local len  = #str
+
+		local peek =
+			function (what)
+				what = what or 1
+
+				if pos > len then
+					return
+				end
+
+				if type(what) == 'string' then
+					return string.match(str, what, pos) or nil
+				end
+
+				return string.sub(str, pos, pos + (what - 1))
+			end
+
+		local read =
+			function (what)
+				local s = peek(what)
+
+				pos = pos  + #s
+
+				return s
+			end
+
+		local ok, result = pcall(impl.read, peek, read)
+
+		if not ok then
+			local tmp  = string.sub(str, 1, pos)
+			local at   = #(string.match(tmp, '[^\n]+$'))
+			local line = select(2, string.gsub(tmp, '\n', '%0')) + 1
+
+			error(string.format('syntax error: line %d, character %d', line, at))
+		end
+
+		return result
+	end
+
+local is_circular =
+	function (val)
+		local seen = {}
+
+		local descend = nil
+		descend =
+			function (t)
+				seen[t] = true
+
+				for _, v in pairs(t) do
+					if type(v) == 'table' then
+						if seen[v] then
+							error()
+						end
+
+						descend(v)
+					end
+				end
+			end
+
+		return not pcall(descend, val)
+	end
+
+jsua.write =
+	function (val)
+		if type(val) == 'table' and is_circular(val) then
+			assert(false, 'object has circular references')
+		end
+
+		local buf = {}
+
+		impl.write_value(val, function (s) table.insert(buf, s) end)
+
+		return table.concat(buf)
+	end
 
 return jsua
